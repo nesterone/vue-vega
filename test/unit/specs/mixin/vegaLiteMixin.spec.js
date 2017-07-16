331dcfc27cdc988919141763cd8bc829a5537121
nesterone <igornester@gmail.com>
2017-07-16 16:59:52 +0300

    - check for descriptionˆ

--- test/unit/specs/mixin/vegaLiteMixin.spec.js
+++ test/unit/specs/mixin/vegaLiteMixin.spec.js
@@ -1,6 +1,6 @@
-import vegaLiteMixin from 'src/mixin/vegaLiteMixin'
-import MarkOptionMissedError from 'src/error/MarkOptionMissedError'
-import EncodingOptionMissedError from 'src/error/EncodingOptionMissedError'
+import vegaLiteMixin from 'src/mixin/vegaLiteMixin';
+import MarkOptionMissedError from 'src/error/MarkOptionMissedError';
+import EncodingOptionMissedError from 'src/error/EncodingOptionMissedError';
 
 describe('vegaLiteMixin', () => {
   describe('beforeCreate', () => {
@@ -50,6 +50,31 @@ describe('vegaLiteMixin', () => {
         mark: 'blabla',
         encoding: {}
       })
+    })
+  })
+  describe('created', () => {
+    it('should add description to spec', () => {
+      const context = {
+        description: 'description',
+        $spec: {
+          data: {
+            values: [
+              {a: 'A', b: 28}, {a: 'B', b: 55}, {a: 'C', b: 43},
+              {a: 'D', b: 91}, {a: 'E', b: 81}, {a: 'F', b: 53},
+              {a: 'G', b: 19}, {a: 'H', b: 87}, {a: 'I', b: 52}
+            ]
+          },
+          mark: 'bar',
+          encoding: {
+            x: {field: 'a', type: 'ordinal'},
+            y: {field: 'b', type: 'quantitative'}
+          }
+        }
+      }
+
+      vegaLiteMixin.created.call(context);
+
+      expect(context.$spec.description).to.equal(context.description)
     })
   })
 })
