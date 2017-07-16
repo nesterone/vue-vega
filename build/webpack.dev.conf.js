36dac428eb26d2b1923dc24b31364ced60c46001
nesterone <igornester@gmail.com>
2017-07-16 21:08:09 +0300

    - add debugble source mapsˆ

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -15,8 +15,8 @@ module.exports = merge(baseWebpackConfig, {
   module: {
     rules: utils.styleLoaders({ sourceMap: config.dev.cssSourceMap })
   },
-  // cheap-module-eval-source-map is faster for development
-  devtool: '#cheap-module-eval-source-map',
+  // maybe it's slower but debug works only with that type
+  devtool: 'source-map',
   plugins: [
     new webpack.DefinePlugin({
       'process.env': config.dev.env
