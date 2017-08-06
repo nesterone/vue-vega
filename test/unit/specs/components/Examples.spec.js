19d2959beffd68ac255f8a5327bc92e7a4243ea2
nesterone <igornester@gmail.com>
2017-08-06 20:07:23 +0300

    Bind data updates in component with vega instance updates #7 - add integration sanity check for new examples

--- test/unit/specs/components/Examples.spec.js
+++ test/unit/specs/components/Examples.spec.js
@@ -1,16 +1,36 @@
 import Vue from 'vue'
 import ComponentAsVegaLiteSpecExample from 'examples/ComponentAsVegaLiteSpecExample'
+import VegaLiteComponentExample from 'examples/VegaLiteComponentExample'
+import UpdateDataExample from 'examples/UpdateDataExample'
 import VueVega from 'src/index'
 
-describe('Docs Examples', () => {
+describe('Examples', () => {
   let LocalVue = Vue
   // TODO: in 'Provide scoped vue instance for testing plugin #16'
   LocalVue.use(VueVega)
 
-  it('should render correct contents', () => {
+  function getAxisText (vm) {
+    return vm.$el.querySelector('.mark-text.role-axis-title text').textContent
+  }
+
+  it('runs sanity check for `component as spec`', () => {
     const Constructor = LocalVue.extend(ComponentAsVegaLiteSpecExample)
     const vm = new Constructor().$mount()
 
-    expect(vm.$el.querySelector('.mark-text.role-axis-title text').textContent).to.equal('a')
+    expect(getAxisText(vm)).to.equal('a')
+  })
+
+  it('runs sanity check for `vega component`', () => {
+    const Constructor = LocalVue.extend(VegaLiteComponentExample)
+    const vm = new Constructor().$mount()
+
+    expect(getAxisText(vm)).to.equal('a')
+  })
+
+  it('runs sanity check for `update data`', () => {
+    const Constructor = LocalVue.extend(UpdateDataExample)
+    const vm = new Constructor().$mount()
+
+    expect(getAxisText(vm)).to.equal('BIN(val)')
   })
 })
