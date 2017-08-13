99be776e52946efd49bec38ac90473e785c03948
nesterone <igornester@gmail.com>
2017-08-13 18:55:38 +0300

    Add mark, description and encoding as properties for components #9 - examples, moved encoding from templates to data

--- _docs/partials/examples/UpdateDataExample.vue
+++ _docs/partials/examples/UpdateDataExample.vue
@@ -3,7 +3,7 @@
     vega-lite(
       mark="bar",
       :data="values",
-      :encoding="{x: {bin: true, field: 'val', type: 'quantitative'}, y: {aggregate: 'count', type: 'quantitative'} }"
+      :encoding="encoding"
     )
     button(@click="refreshNumbers") Refresh
 </template>
@@ -16,7 +16,11 @@
           {val: 1}, {val: 5}, {val: 3},
           {val: 4}, {val: 5}, {val: 6},
           {val: 7}, {val: 8}, {val: 9}
-        ]
+        ],
+        encoding: {
+          x: {bin: true, field: 'val', type: 'quantitative'},
+          y: {aggregate: 'count', type: 'quantitative'}
+        }
       }
     },
     methods: {
--- _docs/partials/examples/VegaLiteComponentExample.vue
+++ _docs/partials/examples/VegaLiteComponentExample.vue
@@ -2,7 +2,7 @@
   vega-lite(
     :data="values",
     mark="bar",
-    :encoding="{ x: {field: 'a', type: 'ordinal'}, y: {field: 'b', type: 'quantitative'} }"
+    :encoding="encoding"
   )
 </template>
 
@@ -14,7 +14,11 @@
           {a: 'A', b: 28}, {a: 'B', b: 55}, {a: 'C', b: 43},
           {a: 'D', b: 91}, {a: 'E', b: 81}, {a: 'F', b: 53},
           {a: 'G', b: 19}, {a: 'H', b: 87}, {a: 'I', b: 52}
-        ]
+        ],
+        encoding: {
+          x: {field: 'a', type: 'ordinal'},
+          y: {field: 'b', type: 'quantitative'}
+        }
       }
     }
   }
