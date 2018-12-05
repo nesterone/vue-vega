fc91841f7b5dfa00af4568892ef61d92c0c1ee30
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2018-12-05 02:01:24 -0400

    Updated packages

--- src/constants.js
+++ src/constants.js
@@ -7,7 +7,7 @@ export const COMPONENT_TEMPLATE = '<div></div>'
 export const VEGA_LITE_COMPONENT_NAME = 'vega-lite'
 export const DEFAULT_DATA_SOURCE_NAME = 'source_0'
 export const SIGNAL_EVENT_PREFIX = 'signal'
-export const VEGA_LITE_SCHEMA_URL = 'https://vega.github.io/schema/vega-lite/v2.json'
+export const VEGA_LITE_SCHEMA_URL = 'https://vega.github.io/schema/vega-lite/v2.6.0.json'
 export const EVENTS_TO_DELEGATE = [
   'keydown',
   'keypress',
